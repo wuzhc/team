f64e29b5c8ec6d1730297311b98e3c5387cac06b
wuzhc <wuzhc2016@163.com>
2018-01-15 11:52:21 +0800

    console config

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=team',
             'username' => 'root',
-            'password' => '',
+            'password' => 'wuzhc2580',
             'charset' => 'utf8',
         ],
         'mailer' => [
