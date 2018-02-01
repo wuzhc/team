b46d400d3166bf0c58dd884ce30ae0e26c387aca
wuzhc <wuzhc2016@163.com>
2018-02-01 21:50:53 +0800

    首页说明

--- frontend/views/default/index.php
+++ frontend/views/default/index.php
@@ -55,34 +55,37 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed201
 
 <div class="row">
     <div class="col-lg-4">
-        <h2>功能说明</h2>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-            fugiat nulla pariatur.</p>
-
-        <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+        <h3>功能说明</h3>
+        <ul class="list-group">
+            <li class="list-group-item"><a target="_blank" href="https://github.com/wuzhc/team/blob/master/docs/%E5%8A%9F%E8%83%BD%E9%9C%80%E6%B1%82.md" class="text-black"><i class="fa fa-car"></i>&nbsp;&nbsp;项目管理</a></li>
+            <li class="list-group-item"><a target="_blank" href="https://github.com/wuzhc/team/blob/master/docs/%E5%8A%9F%E8%83%BD%E9%9C%80%E6%B1%82.md" class="text-black"><i class="fa fa-car"></i>&nbsp;&nbsp;任务管理</a></li>
+            <li class="list-group-item"><a target="_blank" href="https://github.com/wuzhc/team/blob/master/docs/%E5%8A%9F%E8%83%BD%E9%9C%80%E6%B1%82.md" class="text-black"><i class="fa fa-car"></i>&nbsp;&nbsp;用户管理</a></li>
+            <li class="list-group-item"><a target="_blank" href="https://github.com/wuzhc/team/blob/master/docs/%E5%8A%9F%E8%83%BD%E9%9C%80%E6%B1%82.md" class="text-black"><i class="fa fa-car"></i>&nbsp;&nbsp;团队管理</a></li>
+<!--            <li class="list-group-item"><a target="_blank" href="https://github.com/wuzhc/team/blob/master/docs/%E6%B6%88%E6%81%AF%E6%8E%A8%E9%80%81.md" class="text-black"><i class="fa fa-car"></i>&nbsp;&nbsp;消息推送</a></li>-->
+<!--            <li class="list-group-item"><a target="_blank" href="https://github.com/wuzhc/team/blob/master/docs/%E6%9D%83%E9%99%90RBAC%E5%8A%9F%E8%83%BD%E8%AF%B4%E6%98%8E.md" class="text-black"><i class="fa fa-car"></i>&nbsp;&nbsp;权限管理</a></li>-->
+        </ul>
     </div>
     <div class="col-lg-4">
-        <h2>技术说明</h2>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-            fugiat nulla pariatur.</p>
-
-        <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+        <h3>技术说明</h3>
+        <ul class="list-group">
+            <li class="list-group-item"><a target="_blank" href="https://github.com/wuzhc/team/blob/master/docs/%E5%8A%9F%E8%83%BD%E9%9C%80%E6%B1%82.md" class="text-black"><i class="fa fa-motorcycle"></i>&nbsp;&nbsp;RBAC权限功能</a></li>
+            <li class="list-group-item"><a target="_blank" href="http://www.yiichina.com/doc/guide/2.0/db-migrations" class="text-black"><i class="fa fa-motorcycle"></i>&nbsp;&nbsp;数据库迁移</a></li>
+            <li class="list-group-item"><a target="_blank" href="https://segmentfault.com/a/1190000012895446" class="text-black"><i class="fa fa-motorcycle"></i>&nbsp;&nbsp;命令行实现数据库表结构文档自动生成</a></li>
+            <li class="list-group-item"><a target="_blank" href="https://github.com/wuzhc/team/blob/master/docs/%E6%B6%88%E6%81%AF%E6%8E%A8%E9%80%81.md" class="text-black"><i class="fa fa-motorcycle"></i>&nbsp;&nbsp;消息推送和实时动态</a></li>
+<!--            <li class="list-group-item"><a target="_blank" href="https://github.com/wuzhc/team/blob/master/docs/%E5%8A%9F%E8%83%BD%E9%9C%80%E6%B1%82.md" class="text-black"><i class="fa fa-motorcycle"></i>&nbsp;&nbsp;团队管理</a></li>-->
+<!--            <li class="list-group-item"><a target="_blank" href="https://github.com/wuzhc/team/blob/master/docs/%E6%9D%83%E9%99%90RBAC%E5%8A%9F%E8%83%BD%E8%AF%B4%E6%98%8E.md" class="text-black"><i class="fa fa-motorcycle"></i>&nbsp;&nbsp;权限管理</a></li>-->
+        </ul>
     </div>
     <div class="col-lg-4">
-        <h2>参考文档</h2>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-            fugiat nulla pariatur.</p>
-
-        <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+        <h3>相关链接</h3>
+        <ul class="list-group">
+            <li class="list-group-item"><a target="_blank" href="https://github.com/walkor/phpsocket.io" class="text-black"><i class="fa fa-space-shuttle"></i>&nbsp;&nbsp;phpsocket.io</a></li>
+            <li class="list-group-item"><a target="_blank" href="http://www.yiichina.com/doc/guide/2.0" class="text-black"><i class="fa fa-space-shuttle"></i>&nbsp;&nbsp;Yii 2.0 权威指南</a></li>
+            <li class="list-group-item"><a target="_blank" href="https://v3.bootcss.com/" class="text-black"><i class="fa fa-space-shuttle"></i>&nbsp;&nbsp;Bootstrap响应式框架</a></li>
+            <li class="list-group-item"><a target="_blank" href="https://github.com/dmstr/yii2-adminlte-asset" class="text-black"><i class="fa fa-space-shuttle"></i>&nbsp;&nbsp;yii2-adminlte-asset</a></li>
+<!--            <li class="list-group-item"><a target="_blank" href="https://github.com/wuzhc/team/blob/master/docs/%E6%B6%88%E6%81%AF%E6%8E%A8%E9%80%81.md" class="text-black"><i class="fa fa-space-shuttle"></i>&nbsp;&nbsp;消息推送</a></li>-->
+<!--            <li class="list-group-item"><a target="_blank" href="https://github.com/wuzhc/team/blob/master/docs/%E6%9D%83%E9%99%90RBAC%E5%8A%9F%E8%83%BD%E8%AF%B4%E6%98%8E.md" class="text-black"><i class="fa fa-space-shuttle"></i>&nbsp;&nbsp;权限管理</a></li>-->
+        </ul>
     </div>
 </div>
 
